204926e917534c96d90cf7bf5559932d1d3eb5af
cuneytsapanca <49268877+cuneytsapanca@users.noreply.github.com>
2026-07-27 12:46:08 +0300

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE = 'sayarak-v1';
+const URLS = ['./', './index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
